3d87b9c84c9c47662d15a589bdbb1bb270e3cf41
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-08-22 13:59:23 -0500

    Add files via upload

---
+++ printpractice.py
@@ -0,0 +1,11 @@
+# Knock-Knock Joke Program
+print("Knock, Knock.")
+print("who's there?")
+print("dishes.")
+print("dishes who?")
+print("dishes the police, open up!")
+
+print()
+
+#as one line of code with escape characters
+print("knock, knock. \nwho's there? \ndishes.\ndishes who?\ndishes the police, open up!")
