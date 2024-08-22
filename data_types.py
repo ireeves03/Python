e8e305cefa446a08441adaea071e05e83e80a2d5
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-08-22 14:15:14 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 20
+print("Literal:", x)
+
+# Integer
+y = -13
+print("Integer:", y)
+
+# Float
+z = 29.5
+print("Float:", z)
+
+# String
+name = "Bella"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
