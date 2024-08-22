31900ec55a20cf4d15a4d6eff173cb7a777966dd
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-08-22 14:45:42 -0500

    Add files via upload

---
+++ basic-operations.py
@@ -0,0 +1,6 @@
+# Demonstration of mathematical operations and concatenation
+length=6
+width=8
+area= length * width
+
+print("\nThe area of a rectangle with a length of " + str(length) + " " + "and a width of " + str(width) + " " + "is "+ str(area))
