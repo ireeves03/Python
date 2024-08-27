9c8f1332c1ea724e434f8eb02bac10ad1056f138
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-08-27 14:31:10 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+#Setting value for how many gallons in gas tank
+tank_size = 15
+
+#Setting value for distance traveled
+distance = 600
+
+#equation for miles per gallon
+mpg = distance / tank_size
+
+#print out results
+print("Miles per gallon:", mpg)
