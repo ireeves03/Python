1781ee205fbc4741d5693850366a8d9ace39a7b4
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-09-06 11:43:57 -0500

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+count = 99
+while count > 1:
+    print(count, "bottles of beer on the wall!")
+    print(count, "bottles of beer!")
+    print("take one down, pass it around")
+    count -= 1  # Increment count
+if count > 1:
+    print(count, "bottles of beer on the wall!\n")
+elif count: 1
+print(count, "bottle of beer on the wall!\n")
+
+if count: 1
+print(count, "bottle of beer on the wall!")
+print (count, "bottle of beer!")
+print("take one down, pass it around")
+count -= 1  # Increment count
+print(count, "bottles of beer on the wall!")
