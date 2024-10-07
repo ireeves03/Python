9770b62c77c01fb3210105fdc30de76f247a20e9
ireeves03 <156941132+ireeves03@users.noreply.github.com>
2024-10-07 15:04:20 -0500

    Add files via upload

---
+++ Modules/main.py
@@ -0,0 +1,18 @@
+from math_operations import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
+from math_operations import geometry
+
+result = geometry.rectangle(4,8)
+print("the area of the rectangle is ", result)
+
+result = geometry.triangle(9,6)
+print("the area of a triangle is ", result)
+
+result = geometry.circle(3)
+print("the area of the circle is ", result)
---
+++ Modules/math_operations/geometry.py
@@ -0,0 +1,11 @@
+def rectangle (length, width):
+    area = length * width
+    return area
+
+def triangle (length, width):
+    area = length * width
+    return area * 0.5
+
+def circle (radius):
+    area = 3.14 * radius** 2
+    return area
